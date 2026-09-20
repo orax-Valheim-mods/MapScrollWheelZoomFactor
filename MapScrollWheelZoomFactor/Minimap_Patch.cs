8beb745db3ab3295104b69388e297b82821b9dc6
orax <0-F@users.noreply.github.com>
2026-09-20 12:49:14 +0200

    fix: update for Valheim 1.0

--- MapScrollWheelZoomFactor/Minimap_Patch.cs
+++ MapScrollWheelZoomFactor/Minimap_Patch.cs
@@ -1,5 +1,6 @@
 ﻿using HarmonyLib;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Reflection.Emit;
 
 namespace MapScrollWheelZoomFactor;
@@ -10,25 +11,27 @@ public static class Minimap_Patch
     [HarmonyPatch(typeof(Minimap), nameof(Minimap.UpdateMap))]
     public static class UpdateMap_Patch
     {
+        private static readonly MethodInfo LargeZoomGetter = AccessTools.PropertyGetter(typeof(Minimap), nameof(Minimap.LargeZoom));
+
         public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
         {
             /*
              * PATCH:
-             * num = Mathf.Clamp(num, -0.05f, 0.05f) * this.m_largeZoom * 2f;
-             * 
+             * num = Mathf.Clamp(num, -0.05f, 0.05f) * this.LargeZoom * 2f;
+             *
              * AFTER:
-             * num = Mathf.Clamp(num, -0.05f, 0.05f) * this.m_largeZoom * <ZoomMultiplier>;
+             * num = Mathf.Clamp(num, -0.05f, 0.05f) * this.LargeZoom * <ZoomMultiplier>;
              */
 
             CodeMatcher cm = new(instructions);
 
-            cm.MatchForward(true, // false = move at the start of the match, true = move at the end of the match)
+            cm.MatchForward(true, // false = move at the start of the match, true = move at the end of the match
               new CodeMatch(OpCodes.Ldloc_0),
               new CodeMatch(OpCodes.Ldc_R4),
               new CodeMatch(OpCodes.Ldc_R4),
               new CodeMatch(OpCodes.Call),
               new CodeMatch(OpCodes.Ldarg_0),
-              new CodeMatch(OpCodes.Ldfld),
+              new CodeMatch(i => (i.opcode == OpCodes.Call || i.opcode == OpCodes.Callvirt) && (MethodInfo)i.operand == LargeZoomGetter),
               new CodeMatch(OpCodes.Mul),
               new CodeMatch(OpCodes.Ldc_R4),
               new CodeMatch(OpCodes.Mul),
